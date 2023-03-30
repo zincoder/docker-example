cb1f3ffe3960e5c765bebbb1a4d836b8771e0fbb
zinahue <zinahue@naver.com>
2023-03-30 22:38:29 +0900

    first commit

---
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,12 @@
+package hello.hellospring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String home() {
+        return "hello world";
+    }
+}
